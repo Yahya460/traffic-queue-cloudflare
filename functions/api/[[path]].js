e38b0173c9ce64a38e858a1a37834b0d6a97c7bd
yahya <yahya9535@gmail.com>
2025-12-13 22:03:30 +0400

    Restore v4 features

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
   const stub = context.env.QUEUE.getByName("main");
   const req = context.request;
-
   return stub.fetch(new Request(req.url, {
     method: req.method,
     headers: req.headers,
