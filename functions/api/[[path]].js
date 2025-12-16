8f17286a5402386eac62201540fbd1acdfa4ea3c
yahya <yahya9535@gmail.com>
2025-12-16 10:53:33 +0400

    Clean up onRequest function by removing comment
    Removed commented-out line in onRequest function.

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,7 +1,5 @@
 export async function onRequest(context) {
   const { request, env } = context;
-
-  // مرّر الطلب كما هو (بدون إعادة بناء Request)
   const stub = env.QUEUE.getByName("main");
   return stub.fetch(request);
 }
